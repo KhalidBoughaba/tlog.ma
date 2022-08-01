c5c181bfd691e0759071d319f00373da7eda5d1d
KhalidBoughaba <76523347+KhalidBoughaba@users.noreply.github.com>
2022-08-01 14:38:33 +0100

    save
    save

--- tlog/src/app/app.component.ts
+++ tlog/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
+
+//const AOS = require('aos');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'tlog';
+  ngOnInit() {
+    AOS.init();
+  }
 }
